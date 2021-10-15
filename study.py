4bf84b495eb26056d6f5b7389a8b9eb14f1e6e95
Hannah <hannahwl@buffalo.edu>
2021-10-15 17:33:26 -0400

    Added in question choice logic

--- study.py
+++ study.py
@@ -3,24 +3,14 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient import discovery
 from pprint import pprint
-
-
+import random
+import webbrowser
 
 creds = None
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 SERVICE_ACCOUNT_FILE = 'keys.json'
 creds = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-# The ID and range of a sample spreadsheet.
-# SAMPLE_SPREADSHEET_ID = '1YXMMcs73sIU5qZfDkGywbCXyeAXrOImGtWX-xYcMZac'
-# service = build('sheets', 'v4', credentials=creds)
-# # Call the Sheets API
-# sheet = service.spreadsheets()
-# result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-#                             range="170Questions!A1:G10", valueRenderOption='FORMULA').execute()
-# values = result.get('values', [])
-
-
 
 
 service = discovery.build('sheets', 'v4', credentials=creds)
@@ -28,24 +18,56 @@ spreadsheet_id = '1YXMMcs73sIU5qZfDkGywbCXyeAXrOImGtWX-xYcMZac'  # TODO: Update 
 
 ranges = ["170Questions!G2:G3"]  # TODO: Update placeholder value.
 
-# True if grid data should be returned.
-# This parameter is ignored if a field mask was set in the request.
 include_grid_data = True  # TODO: Update placeholder value.
 
 request = service.spreadsheets().get(spreadsheetId=spreadsheet_id, ranges=ranges, includeGridData=include_grid_data)
 response = request.execute()
 values = response['sheets'][0]['data'][0]['rowData']
+count = 0
+easy = []
+medium = []
+hard = []
 for row in values:
-    test = row['values']
-    for val in test:
-        print(val['hyperlink'])
-# print(values)
-# pprint(response)
-# print(values)
-# print(values[2][6])
-# # sentences = []
-# for val in values:
-#     if len(val) > 0:
-#         sentences.append(val[0])
-# df = px.data.gapminder().query("continent=='Oceania'")
+	link = row['values'][0]['hyperlink']
+	if count < 35:
+		easy.append(link)
+	elif count <140:
+		medium.append(link)
+	else:
+		hard.append(link)
+	count += 1
 
+num_easy = input("Enter number of easy questions (max 35): ")
+while not num_easy.isnumeric() or (int(num_easy) < 0 or int(num_easy) > 35):
+	num_easy = input("Please enter a valid number (0-35): ")
+
+num_medium = input("Enter number of medium questions (max 105): ")
+while not num_medium.isnumeric() or (int(num_medium) <0 or int(num_medium) > 105):
+	num_medium = input("Please enter a valid number (0-105): ")
+
+num_hard = input("Enter number of hard questions (max 30): ")
+while not num_hard.isnumeric() or (int(num_hard) <0 or int(num_hard) > 30):
+	num_hard = input("Please enter a valid number (0-30): ")
+
+num_easy = int(num_easy)
+num_medium = int(num_medium)
+num_hard = int(num_hard)
+print(num_easy)
+print(num_medium)
+print(num_hard)
+
+
+easy_quest = random.choice(easy, k=num_easy)
+medium_quest = random.choice(medium, k=num_medium)
+hard_quest = random.choice(hard, k=num_hard)
+
+for quest in easy_quest:
+	webbrowser.open(quest)
+
+for quest in medium_quest:
+	webbrowser.open(quest)
+
+for quest in hard_quest:
+	webbrowser.open(quest)
+
+print("Happy Studying :D")
